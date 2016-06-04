199a2beee4087c323f49d5d11a769a41921a2541
Alex <alexdkaiser@gmail.com>
2016-06-04 15:44:58 -0700

    Following tutorial

--- app/src/main/java/com/realitaid/pyrobug/realitaidv1/Realitaid.java
+++ app/src/main/java/com/realitaid/pyrobug/realitaidv1/Realitaid.java
@@ -1,13 +1,27 @@
 package com.realitaid.pyrobug.realitaidv1;
 
+import android.content.Intent;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Realitaid extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.realitaid.pyrobug.realitaidv1.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_realitaid);
+        setContentView(R.layout.activity_formtest);
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.testinput);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
