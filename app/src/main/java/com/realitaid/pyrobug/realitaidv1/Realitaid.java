cb7143a55c74a336a8d2addefafa660c74b421b5
Bnocular <housenuker@EDI.local>
2016-06-04 14:25:46 -0700

    Initial commit

---
+++ app/src/main/java/com/realitaid/pyrobug/realitaidv1/Realitaid.java
@@ -0,0 +1,13 @@
+package com.realitaid.pyrobug.realitaidv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Realitaid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realitaid);
+    }
+}
